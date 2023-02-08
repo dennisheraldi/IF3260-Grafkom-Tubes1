68e7788abc3a0104107387e9cf7078aa247247d6
Fachry Dennis Heraldi <71638224+dennisheraldi@users.noreply.github.com>
2023-02-08 08:22:00 +0700

    fix: bug on fill color inside the rectangle

--- src/app.js
+++ src/app.js
@@ -257,18 +257,31 @@ function fillMouseDownHandler(e) {
     var x_down = ((e.clientX - rect.left) / rect.width) * 2 - 1;
     var y_down = ((e.clientY - rect.top) / rect.height) * -2 + 1;
 
-    function distance(x1, y1, x2, y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-    }
-
     // check for each rectangle
     for (var i = 0; i < object.rectangle.positions.length; i++) {
         var position = object.rectangle.positions[i];
         // check for each point in the rectangle
         for (var p = 0; p < position.length; p++) {
             if (
-                distance(x_down, y_down, position[p * 2], position[p * 2 + 1]) <
-                0.1
+                (distance(
+                    x_down,
+                    y_down,
+                    position[p * 2],
+                    position[p * 2 + 1]
+                ) <
+                    0.2) &
+                pointIsInPoly(
+                    {
+                        x: x_down,
+                        y: y_down,
+                    },
+                    [
+                        { x: position[0], y: position[1] },
+                        { x: position[2], y: position[3] },
+                        { x: position[4], y: position[5] },
+                        { x: position[6], y: position[7] },
+                    ]
+                )
             ) {
                 // fill the color for corresponding point
                 object.rectangle.colors[i][p * 3] = currentColor[0];
@@ -326,13 +339,8 @@ function pointIsInPoly(p, polygon) {
     return isInside;
 }
 
-console.log(
-    pointIsInPoly({ x: 0.5, y: 0.5 }, [
-        { x: 0, y: 0 },
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 0, y: 1 },
-    ])
-);
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
 
 window.onload = main;
