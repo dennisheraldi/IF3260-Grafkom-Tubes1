f5bee23639cf870f5b5c918c6dca49ec61586c92
hafidznrg <hafidznur.hn@gmail.com>
2023-02-25 21:39:23 +0700

    feat: add vertex convex hull

--- src/tools/addVertex.js
+++ src/tools/addVertex.js
@@ -13,26 +13,34 @@ function addVertexButtonHandler() {
 function vertexMouseDownHandler(e) {
   var x_down = getMousePosition(e)[0];
   var y_down = getMousePosition(e)[1];
-  console.log("x:", x_down, "y:", y_down);
 
-  if (shapeNow == null) {
-    shapeNow = findShape(x_down, y_down);
-    console.log(shapeNow);
+  if (vertexNow == null || vertexNow[0] != "polygon") {
+    vertexNow = findVertex(x_down, y_down);
+    console.log(vertexNow)
   } else {
-    if (vertexNow == null) {
-      vertexNow = findVertex(x_down, y_down);
-      console.log(vertexNow);
-    } else {
-      var shapeType = shapeNow[0];
-      var shapeIndex = shapeNow[1];
-      var vertexIndex = vertexNow[2];
-      if (shapeType == "polygon") {
-        // add vertex positions and colors
-        object.polygon.positions[shapeIndex].splice((vertexIndex + 1) * 2, 0, x_down, y_down);
-        object.polygon.colors[shapeIndex].splice((vertexIndex + 1) * 3, 0, ...currentColor);
-        shapeNow = null;
-        vertexNow = null;
+    var shapeType = vertexNow[0];
+    var shapeIndex = vertexNow[1];
+    var vertexIndex = vertexNow[2];
+      // add vertex positions and colors
+
+    polygonPosition = object.polygon.positions[shapeIndex];
+    polygonColor = object.polygon.colors[shapeIndex];
+    polygonPosition.splice((vertexIndex + 1) * 2, 0, x_down, y_down);
+    polygonColor.splice((vertexIndex + 1) * 3, 0, ...currentColor);
+
+    if (isPolygonConvexHull) {
+      polygonPosition = convexHull(polygonPosition);
+      // color filling
+      polygonColor = [];
+      for (var i = 0; i < polygonPosition.length / 2; i++) {
+        polygonColor.push(...currentColor);
       }
-    }
+    } 
+    object.polygon.positions[shapeIndex] = polygonPosition;
+    object.polygon.colors[shapeIndex] = polygonColor;
+    polygonPosition = []
+    polygonColor = []
+    shapeNow = null;
+    vertexNow = null;
   }
 }
